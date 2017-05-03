3229549adbb8b102ea1052f75d2941b398a70d7b
Haidee <haideram97@gmail.com>
2017-05-02 22:39:22 -0500

    agregando reporte

--- materialize/js/pedidos.js
+++ materialize/js/pedidos.js
@@ -2,6 +2,7 @@
 var checkboxes = document.getElementsByName("checkbox");
 console.log(checkboxes);
 //Obtención del elemento  HTML con id#
+//Cajas de contenido donde se alamcenaran los pedidos de las entradasm, plato fuerte, postres y bebidas.
 var cajaEntradas = document.getElementById("pedidoEntrada");
 var cajaPlatoFuerte = document.getElementById("pedidoPlatoFuerte");
 var cajaPostres = document.getElementById("pedidoPostre");
@@ -14,7 +15,7 @@ function agregarPedidos() {
 	var platofuerte = [];
 	var postres = [];
 	var bebidas = [];
-	//Arrays para almacenar los precios de el pedido por categoría: entrada, plato fuerte, postre, bebidas
+	//Arrays para almacenar los precios del pedido por categoría: entrada, plato fuerte, postre, bebidas.
 	var preciosEntradas = [];
 	var preciosPlatoFuerte = [];
 	var preciosPostres = [];
@@ -26,16 +27,16 @@ function agregarPedidos() {
 	var contB = 0;
 	//Variable de suma para almacenar el total a pagar por parte del cliente.
 	var suma = 0;
+	//Reiniciar el contenido de las cajas donde se almacena la orden.
 	cajaEntradas.innerHTML="";
 	cajaPlatoFuerte.innerHTML="";
 	cajaPostres.innerHTML="";
 	cajaBebidas.innerHTML="";
 	cajaTotal.innerHTML="";
-	console.log('antes del ciclo ');
 	//Ciclo para o recorrer el arreglo de los checkbox
 	for (var x=0; x < checkboxes.length; x++) {	
-		console.log('entre al ciclo for');
-		//Evaluar el checkbox del arreglo checkboxes en la posición x esta clickeado, es decir su atributo checked es igual a true.
+		/*Evaluar el checkbox del arreglo checkboxes en la posición x esta clickeado, 
+		es decir su atributo checked es igual a true.*/
 		if (checkboxes[x].checked) {
 			//Según la clase del checkbox, este será almacenado en un array de acuerdo a su categoría.
 			switch (checkboxes[x].className) { 
@@ -46,7 +47,6 @@ function agregarPedidos() {
 					break;
 				case 'platofuerte':
 					platofuerte[contPF] = checkboxes[x].id;
-					console.log('entro en plato fuerte');
 					preciosPlatoFuerte[contPF] = parseInt(checkboxes[x].value);	
 					contPF=contPF+1;	
 					break;
@@ -65,6 +65,11 @@ function agregarPedidos() {
 			}
 		}
 	}
+	//Ciclos para recorrer los arreglos: entradas[], platofuerte[], postres[] y bebidas[].
+	/*En el recorrido del array en cada iteración se crean elementos de parrafó (p) 
+	para poder colocar texto a cada uno de ellos, este texto sera el platillo y precio que el cliente pidió 
+	en cada categoría*/
+
 	for (var i=0; i<entradas.length; i++) {		
 		var p = document.createElement("p");
 		p.innerHTML = ""+entradas[i]+": $"+preciosEntradas[i]+".00";
@@ -85,7 +90,8 @@ function agregarPedidos() {
 		p.innerHTML = ""+bebidas[i]+": $"+preciosBebidas[i]+".00";
 		cajaBebidas.appendChild(p);
 	}
-	
+	/*Ciclos para recorrer los arreglos de precios de cada categoría 
+	para realizar la suma total a pagar por el cliente.*/
 	for (var i=0; i<preciosEntradas.length; i++) {
 		suma=suma+preciosEntradas[i];
 	}
@@ -98,6 +104,7 @@ function agregarPedidos() {
 	for (var i=0; i<preciosBebidas.length; i++) {
 		suma=suma+preciosBebidas[i];
 	}
+	//Total a pagar, agregado a un elemento h5, mismo que se agregará a la cajaTotal.
 	var h5 = document.createElement("h5");
 	h5.innerHTML = "$"+suma+".00";
 	cajaTotal.appendChild(h5);
